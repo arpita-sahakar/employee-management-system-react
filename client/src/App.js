be828f806cadf5d60044f62b09d10c78141c2877
Arpita Saha <arpita.sahaa@gmail.com>
2021-06-30 10:05:17 -0400

    create basic ui structer

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import CreateEmployee from "./components/CreateEmployee";
+import EmployeeList from "./components/EmployeeList";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import { Container } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
 
 function App() {
+  const classes = useStyles();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Paper className={classes.paper}> <h1>Employees Create & Show</h1></Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper className={classes.paper}><EmployeeList /></Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper className={classes.paper}><CreateEmployee /></Paper>
+        </Grid>
+      </Grid>
+       
+        
+        
+      </Container>
     </div>
   );
 }
---
+++ client/src/components/CreateEmployee.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function CreateEmployee() {
+    return (
+        <div>
+            <h1>Hello</h1>
+        </div>
+    )
+}
+
+export default CreateEmployee
+
+
